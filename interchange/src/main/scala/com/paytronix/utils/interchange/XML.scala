433eeef2c2498c10afae12c83ae132dd0f200567
Ross Mellgren <rmellgren@paytronix.com>
2012-10-29 11:50:52 -0400

    [issue23822] XML <=> JSON conversion which supports explicit arrays, empty arrays, empty objects, and other things

---
+++ interchange/src/main/scala/com/paytronix/utils/interchange/XML.scala
@@ -0,0 +1,181 @@
+//
+// Copyright 2012 Paytronix Systems, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package com.paytronix.utils.interchange
+
+import scala.annotation.tailrec
+import scala.collection.breakOut
+import scala.collection.mutable.{Builder, ListBuffer}
+import scala.xml.{Comment, Elem, MetaData, Node, NodeSeq, Null, PCData, PrefixedAttribute, Text, TopScope, UnprefixedAttribute}
+
+import net.liftweb.json.JsonAST.{JArray, JBool, JDouble, JField, JInt, JNothing, JNull, JObject, JString, JValue}
+
+/**
+ * Utilities to convert JSON to XML and back again, used to support XML over HTTP instead of the (preferred) JSON over HTTP using Interchange.
+ *
+ * All scalar values (bools, strings, ints, doubles) become text fragments:
+ *   true <=> true, "foobar" <=> foobar, 123 <=> 123, 12.34 <=> 12.34
+ * Except if it's a string value starting or ending with a space, in which case a PCDATA is emitted.
+ * Arrays are reified as <array><item>first</item><item>second</item></array>:
+ *   [1,2,3] <=> <array><item>1</item><item>2</item><item>3</item></array>
+ * Nulls are reified as <null/>
+ *   null <=> <null/>
+ * Objects are implicitly created for any element content that consists of tags that aren't special (<null/>, <array/>):
+ *   {"foo": "bar", "baz": null, "zippy": 123} => <foo>bar</foo><baz><null/></baz><zippy>123</zippy>
+ * As a special case, attributes will be added to a generated object:
+ *   <foo bar="baz"><zippy>123</zippy></foo> => {"foo": {"bar": "baz", "zippy": "123"}}
+ * Empty objects are represented by the lone tag <empty/>:
+ *   {} <=> <empty/>
+ *
+ * Note that since XML has no type information for scalar values all JSON scalars are turned into strings. Interchange supports strings as input for all
+ * basic scalar types, so this shouldn't pose a problem. If you use XML support, you must ensure that any custom coders you use support strings even where integers,
+ * floats, etc are preferred.
+ */
+object XML {
+    /** Convert JSON to XML */
+    def fromJSON(in: JValue): NodeSeq =
+        in match {
+            case JArray(elements) => <array>{ elements.map(e => <item>{ fromJSON(e) }</item>)(breakOut) }</array>
+            case JBool(b)         => Text(b.toString)
+            case JDouble(d)       => Text(d.toString)
+            case JField(n, v)     => Elem(null, n, Null, TopScope, fromJSON(v): _*)
+            case JInt(i)          => Text(i.toString)
+            case JNothing         => NodeSeq.Empty
+            case JNull            => <null />
+            case JObject(Nil)     => <empty />
+            case JObject(fields)  => fields.flatMap(fromJSON)
+            case JString("")      => NodeSeq.Empty
+            case JString(s) if Character.isWhitespace(s.charAt(0)) || Character.isWhitespace(s.charAt(s.length-1)) =>
+                PCData(s)
+            case JString(s) => Text(s)
+        }
+
+    /** Convert XML to JSON */
+    def toJSON(in: NodeSeq): JValue = {
+        def isWhitespace(in: Node): Boolean =
+            in match {
+                case Text(s) => s.trim() == ""
+                case _: Comment => true
+                case _ => false
+            }
+
+        def trim(ns: Seq[Node]): NodeSeq = trimRight(trimLeft(ns))
+
+        def trimLeft(ns: Seq[Node]): NodeSeq =
+            ns.dropWhile(isWhitespace)
+
+        @tailrec
+        def trimRight(ns: Seq[Node]): NodeSeq =
+            if (ns.lastOption.map(isWhitespace).getOrElse(false))
+                trimRight(ns.dropRight(1))
+            else
+                ns
+
+        def textValue(ns: Seq[Node], withTrimming: Boolean): String = {
+            val stringAtoms = ns.filter { case (_: Text)|(_: PCData) => true; case _ => false }
+
+            def trimLeftString(in: String): String =
+                in.indexWhere(ch => !Character.isWhitespace(ch)) match {
+                    case -1 => ""
+                    case pos => in.substring(pos)
+                }
+
+            def trimRightString(in: String): String =
+                in.lastIndexWhere(ch => !Character.isWhitespace(ch)) match {
+                    case -1 => ""
+                    case pos => in.substring(0, pos+1)
+                }
+
+            def rawTextValue(in: Seq[Node]): String =
+                in.view.map {
+                    case Text(s)   => s
+                    case PCData(s) => s
+                    case _ => ""
+                }.mkString
+
+            if (!withTrimming)
+                rawTextValue(stringAtoms)
+            else
+                // ugh, there's probably a better way. also this doesn't correctly handle Text(whitespace) ++ Text(with leading spaces) correctly (it leaves the leading spaces)
+                stringAtoms match {
+                    case Seq(Text(s))   => s.trim
+                    case Seq(PCData(s)) => s
+                    case other =>
+                        val mid = rawTextValue(other.tail.dropRight(1))
+                        (other.head, other.last) match {
+                            case (Text(s),   Text(s2)  ) => trimLeftString(s) + mid + trimRightString(s2)
+                            case (Text(s),   PCData(s2)) => trimLeftString(s) + mid + s2
+                            case (PCData(s), Text(s2)  ) => s                 + mid + trimRightString(s2)
+                            case _ => sys.error("only Text and PCData should be here due to filter")
+                        }
+                }
+        }
+
+        def fieldsFromAttributes(attrs: MetaData): Seq[JField] =
+            attrs.view.map {
+                case PrefixedAttribute(_, name, value, _) => JField(name, JString(textValue(trim(value), false)))
+                case UnprefixedAttribute(name, value, _)  => JField(name, JString(textValue(trim(value), false)))
+                case Null                                 => sys.error("didn't expect Null in attribute view")
+            }.toSeq
+
+        def fixupElementValue(attrs: MetaData, children: Seq[Node]): JValue =
+            (toJSON(children), fieldsFromAttributes(attrs)) match {
+                case (JNothing, Seq())          => JString("")
+                case (JNothing, fields)         => JObject(fields.toList)
+                case (jvalue, Seq())            => jvalue
+                case (JObject(fields), fields2) => JObject(fields ++ fields2)
+                case (jvalue, _) =>
+                    sys.error("unexpected attributes " + attrs + " cannot be added to scalar or array value enclosed (" + jvalue + ")")
+            }
+
+        val ns = trimLeft(in.view)
+        lazy val noMore = trimLeft(ns.tail).isEmpty
+        lazy val allText = ns.forall(n => n.isInstanceOf[Text] || n.isInstanceOf[PCData])
+
+        if (ns.isEmpty)
+            JNothing
+        else
+            ns.head match {
+                case (_: Text)|(_: PCData) if allText =>
+                    JString(textValue(trimRight(ns), true))
+
+                case (_: Text)|(_: PCData) =>
+                    sys.error("unexpected mix of text and other nodes: " + ns)
+
+                case <empty /> if noMore => JObject(Nil)
+                case <null /> if noMore => JNull
+                case Elem(_, "array", _, _, children@_*) if noMore =>
+                    JArray(children.flatMap {
+                        case Elem(_, "item", attrs, _, children@_*) =>
+                            Some(fixupElementValue(attrs, children))
+                        case _ =>
+                            None
+                    } (breakOut))
+
+                case _ =>
+                    JObject(ns.flatMap {
+                        case Elem(_, field, attrs, _, children@_*) =>
+                            Some(JField(field, fixupElementValue(attrs, children)))
+                        case other if isWhitespace(other) =>
+                            None
+                        case other =>
+                            sys.error("unexpected " + other + " (of type " + other.getClass.getName + ") mixed in with elements in " + ns)
+                    } (breakOut))
+            }
+    }
+}
+
+
---
+++ interchange/src/test/scala/com/paytronix/utils/interchange/test/XMLSpecs.scala
@@ -0,0 +1,106 @@
+//
+// Copyright 2012 Paytronix Systems, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package com.paytronix.utils.interchange.test
+
+import scala.xml._
+
+import net.liftweb.json.Implicits._
+import net.liftweb.json.JsonAST._
+import net.liftweb.json.JsonDSL._
+import org.specs2.SpecificationWithJUnit
+
+import com.paytronix.utils.interchange.XML.{fromJSON, toJSON}
+
+class JSONToXMLSpecTest extends SpecificationWithJUnit { def is =
+    "JSON to XML conversion" ^
+    "simple string"               ! { fromJSON(JString("foobar")) must_== Text("foobar") } ^
+    "empty string"                ! { fromJSON(JString("")) must_== NodeSeq.Empty } ^
+    "strings with leading spaces" ! { fromJSON(JString("    foo")) must_== PCData("    foo") } ^
+    "boolean"                     ! { fromJSON(JBool(true)) must_== Text("true") } ^
+    "int"                         ! { fromJSON(JInt(BigInt(123))) must_== Text("123") } ^
+    "double"                      ! { fromJSON(JDouble(12.34)) must_== Text("12.34") } ^
+    "null"                        ! { fromJSON(JNull) must_== <null /> } ^
+    "nothing"                     ! { fromJSON(JNothing) must_== NodeSeq.Empty } ^
+    "empty array"                 ! { fromJSON(JArray(Nil)) must_== <array /> } ^
+    "nonempty array"              ! { fromJSON(List(1,2,3)) must_== <array><item>1</item><item>2</item><item>3</item></array> } ^
+    "empty object"                ! { fromJSON(JObject(Nil)) must_== <empty /> } ^
+    "nonempty object"             ! { fromJSON(("foo" -> "bar") ~ ("baz" -> "qux")) must_== (<foo>bar</foo> ++ <baz>qux</baz>) } ^
+    "nontrivial object" ! {
+        fromJSON (
+            ("enforceUniqueFields" -> JArray(Nil)) ~
+            ("setAccountFields" -> JObject(Nil)) ~
+            ("setUserFields" -> (
+                ("style" -> "typed") ~
+                ("firstName" -> List("Ross")) ~
+                ("lastName" -> JNull)
+            ))
+        ) must_== (
+            <enforceUniqueFields><array /></enforceUniqueFields> ++
+            <setAccountFields><empty /></setAccountFields> ++
+            <setUserFields><style>typed</style><firstName><array><item>Ross</item></array></firstName><lastName><null /></lastName></setUserFields>
+        )
+    }
+}
+
+class XMLToJSONSpecTest extends SpecificationWithJUnit { def is =
+    "XML to JSON conversion" ^
+    "simple string"                                   ! { toJSON(Text("foobar")) must_== JString("foobar") } ^
+    "empty string at top level"                       ! { toJSON(Text("")) must_== JNothing } ^
+    "string with leading spaces (which are trimmed)"  ! { toJSON(Text("    foo")) must_== JString("foo") } ^
+    "string with trailing spaces (which are trimmed)" ! { toJSON(Text("foo    ")) must_== JString("foo") } ^
+    "string with medial spaces"                       ! { toJSON(Text("    foo") ++ Text(" ") ++ Text("bar    ")) must_== JString("foo bar") } ^
+    "PCData with leading spaces"                      ! { toJSON(Text("    ") ++ PCData("  foo")) must_== JString("  foo") } ^
+    "PCData with trailing spaces"                     ! { toJSON(PCData("  foo  ") ++ Text("    ")) must_== JString("  foo  ") } ^
+    "mixed text and PCData"                           ! { toJSON(Text("foo") ++ PCData("  foo  ") ++ Text("bar")) must_== JString("foo  foo  bar") } ^
+    "null"                                            ! { toJSON(<null />) must_== JNull } ^
+    "nothing"                                         ! { toJSON(NodeSeq.Empty) must_== JNothing } ^
+    "empty array"                                     ! { toJSON(<array />) must_== JArray(Nil) } ^
+    "nonempty array"                                  ! { toJSON(<array><item>1</item><item>2</item></array>) must_== JArray(List(JString("1"), JString("2"))) } ^
+    "empty object"                                    ! { toJSON(<empty />) must_== JObject(Nil) } ^
+    "nonempty object"                                 ! { toJSON(<foo>bar</foo> ++ <baz>qux</baz>) must_== ((("foo" -> "bar") ~ ("baz" -> "qux")): JObject) } ^
+    "not treat keywords specially with other fields"  ! { toJSON(<array>123</array> ++ <empty/>) must_== ((("array" -> "123") ~ ("empty" -> "")): JObject) } ^
+    "ignore comments"                                 ! { toJSON(Comment("foo")) must_== JNothing } ^
+    "ignore comments (leading)"                       ! { toJSON(Comment("foo") ++ <empty />) must_== JObject(Nil) } ^
+    "ignore comments (trailing)"                      ! { toJSON(<empty /> ++ Comment("foo")) must_== JObject(Nil) } ^
+    "ignore whitespace"                               ! { toJSON(Text("   \n\r")) must_== JNothing } ^
+    "ignore whitespace (leading)"                     ! { toJSON(Text("  \n\r   ") ++ <empty />) must_== JObject(Nil) } ^
+    "ignore whitespace (trailing)"                    ! { toJSON(<empty /> ++ Text("   \n\r  ")) must_== JObject(Nil) } ^
+    "empty string in value positions"                 ! { toJSON(<foo />) must_== JObject(List(JField("foo", JString("")))) } ^
+    "attributes on array items (implicit)"            ! { toJSON(<array><item foo="bar" baz="qux" /></array>) must_== JArray(List(("foo" -> "bar") ~ ("baz" -> "qux"))) } ^
+    "attributes on array items (explicit)"            ! { toJSON(<array><item foo="bar" baz="qux"><empty/></item></array>) must_== JArray(List(("foo" -> "bar") ~ ("baz" -> "qux"))) } ^
+    "attributes on field values (implicit)"           ! { toJSON(<monkey foo="bar" baz="qux" />) must_== (("monkey" -> (("foo" -> "bar") ~ ("baz" -> "qux"))): JObject) } ^
+    "attributes on field values (explicit)"           ! { toJSON(<monkey foo="bar" baz="qux"><empty /></monkey>) must_== (("monkey" -> (("foo" -> "bar") ~ ("baz" -> "qux"))): JObject) } ^
+    "nontrivial XML" ! {
+        toJSON (
+            <enforceUniqueFields><array /></enforceUniqueFields> ++
+            <setAccountFields><empty /></setAccountFields> ++
+            <setUserFields><style>typed</style><firstName><array><item>Ross</item></array></firstName><lastName><null /></lastName></setUserFields>
+        ) must_== (
+            (
+                ("enforceUniqueFields" -> JArray(Nil)) ~
+                ("setAccountFields" -> JObject(Nil)) ~
+                ("setUserFields" -> (
+                    ("style" -> "typed") ~
+                    ("firstName" -> List("Ross")) ~
+                    ("lastName" -> JNull)
+                ))
+            ): JObject
+        )
+    }
+}
+
+
