3f2cdd8ae47c9827118b78978b81b447a52107d9
Matt Explosion <mexplosion@paytronix.com>
2018-09-27 09:47:45 -0400

    [PXS-53612] Fixed up "classic" date formatter to render with a space and without Z when in UTC

--- interchange/base/src/main/scala/com/paytronix/utils/interchange/base/datetime/datetime.scala
+++ interchange/base/src/main/scala/com/paytronix/utils/interchange/base/datetime/datetime.scala
@@ -149,11 +149,11 @@ object iso8601 extends DateTimeStringBijections (
     dateTimeFormatters = NonEmptyList (
         // ISO_OFFSET_DATETIME doesn't print seconds or millis at all if they're zero, but the old Joda Time code did so we need this for backwards compatibility when rendering datetimes
         DateTimeFormatter.ofPattern("uuuu-MM-dd'T'HH:mm:ss.SSSXXX"),
-        DateTimeFormatter.ofPattern("uuuu-MM-dd'T'HH:mm:ss[.SSS][ ]XXX"),
-        DateTimeFormatter.ofPattern("uuuu-MM-dd'T'HH:mm:ss[.SSS][ ]XX"), // This is required to allow offsets without a colon, e.g. +0100
+        DateTimeFormatter.ofPattern("uuuu-MM-dd'T'HH:mm:ss[.SSS][ ]XX"),  // Offset with no colon, e.g. +0100
+        DateTimeFormatter.ofPattern("uuuu-MM-dd'T'HH:mm:ss[.SSS][ ]XXX"), // Offset with colon, e.g. -05:00
         DateTimeFormatter.ISO_OFFSET_DATE_TIME, // Seems to be equivalent to: uuuu-MM-dd'T'HH:mm:[ss][.SSS]XXX
-        DateTimeFormatter.ofPattern("uuuuMMdd'T'HHmmss[.SSS][ ]XX"), // ISODateTimeFormat.basicDateTime.withOffsetParsed,         -- yyyyMMdd'T'HHmmss.SSSZ
-        DateTimeFormatter.ofPattern("uuuuMMdd'T'HHmmss[.SSS][ ]XXX"), // ISODateTimeFormat.basicDateTime.withOffsetParsed,         -- yyyyMMdd'T'HHmmss.SSSZ
+        DateTimeFormatter.ofPattern("uuuuMMdd'T'HHmmss[.SSS][ ]XX"),
+        DateTimeFormatter.ofPattern("uuuuMMdd'T'HHmmss[.SSS][ ]XXX"),
         DateTimeFormatter.ofPattern("uuuu-MM-dd HH:mm:ss[.SSS][ ]XX"),
         DateTimeFormatter.ofPattern("uuuu-MM-dd HH:mm:ss[.SSS][ ]XXX")
     ),
@@ -177,8 +177,12 @@ object iso8601 extends DateTimeStringBijections (
 
 object classic extends DateTimeStringBijections (
     dateTimeFormatters = NonEmptyList (
+        // This one gets used when rendering dates, so it needs to be like this even though the next one would successfully parse anything this would accept
+        DateTimeFormatter.ofPattern("uuuu-MM-dd HH:mm:ss xx"),
         DateTimeFormatter.ofPattern("uuuu-MM-dd HH:mm:ss[.SSS][ ]XX"),
         DateTimeFormatter.ofPattern("uuuu-MM-dd HH:mm:ss[.SSS][ ]XXX"),
+        DateTimeFormatter.ofPattern("uuuu-MM-dd'T'HH:mm:ss[.SSS][ ]XX"),
+        DateTimeFormatter.ofPattern("uuuu-MM-dd'T'HH:mm:ss[.SSS][ ]XXX"),
         DateTimeFormatter.ofPattern("E MMM dd HH:mm:ss[.SSS] X uuuu"),
         DateTimeFormatter.ofPattern("E, dd MMM yy HH:mm:ss[.SSS] XX"),
         DateTimeFormatter.ofPattern("E, dd MMM yy HH:mm:ss[.SSS] XXX"),
@@ -187,8 +191,7 @@ object classic extends DateTimeStringBijections (
         DateTimeFormatter.ofPattern("uuuuMMdd'T'HHmmss[.SSS][ ]XXX"),
         // For some terrible reason our documentation says timezone is optional so we'll fall back to this
         DateTimeFormatter.ofPattern("uuuu-MM-dd HH:mm:ss[.SSS]").withZone(ZoneOffset.UTC),
-        DateTimeFormatter.ofPattern("uuuu-MM-dd HH:mm:ss[.SSS]").withZone(ZoneOffset.UTC),
-        DateTimeFormatter.ofPattern("uuuu-MM-dd'T'HH:mm:ss[.SSS][ ]XX")
+        DateTimeFormatter.ofPattern("uuuu-MM-dd HH:mm:ss[.SSS]").withZone(ZoneOffset.UTC)
     ),
     localDateFormatters = NonEmptyList (
         DateTimeFormatter.ofPattern("uuuu-MM-dd"),    // DateTimeFormat.forPattern("yyyy-MM-dd"),
