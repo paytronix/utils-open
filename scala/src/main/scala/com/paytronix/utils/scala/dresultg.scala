2e0ac593c68e84b9d5136a232d18ddc2d9c0cedf
jjkv <jjkernsv@gmail.com>
2021-01-29 16:54:04 -0500

    [no issue] dresultg impl start

---
+++ scala/src/main/scala/com/paytronix/utils/scala/dresultg.scala
@@ -0,0 +1,76 @@
+//
+// Copyright 2012-2020 Paytronix Systems, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package com.paytronix.utils.scala
+import result._
+
+object dresultg {
+
+	type DResultG[-D, +E, +A] = D => ResultG[E, A]
+    object DResultG {
+        def liftF[E, A](r: => ResultG[E, A]): DResultG[Any, E, A] = _ => r
+        def pure[A](a: A): DResultG[Any, Nothing, A] = DOkay(a)
+        def apply[D, E, A](f: D => ResultG[E, A]): DResultG[D, E, A] = f
+    }
+
+    type DOkay[+A] = DResultG[Any, Nothing, A]
+    object DOkay {
+        def apply[A](a: A): DResultG[Any, Nothing, A] = _ => Okay(a)
+        def unit: DOkay[Unit] = apply(())
+    }
+
+    type DFailedG[+E] = DResultG[Any, E, Nothing]
+    object DFailedG {
+        def apply[E](e: E): DResultG[Any, E, Nothing] = _ => FailedG[E]("", e)
+    }
+
+    implicit class DFailedGCapabilities[+E](val f: DFailedG[E]) extends AnyVal {
+        def when[e >: E](b: Boolean): DResultG[Any, e, Unit] = 
+            if (b) f.asInstanceOf[DResultG[Any, e, Unit]] else DOkay.unit
+
+        def unless[e >: E](b: Boolean): DResultG[Any, e, Unit] = 
+            when(!b)
+    }
+
+    implicit class DResultCapabilities[-D, +E, +A](val fa: DResultG[D, E, A]) extends AnyVal {
+        def flatMap[DD <: D, e >: E, B](f: A => DResultG[DD, e, B]): DResultG[DD, e, B] = 
+            dd => fa(dd).flatMap(a => f(a)(dd))
+
+        def map[B](f: A => B): DResultG[D, E, B] = 
+            d => fa(d).map(f)
+
+        def filter[e >: E : FailedParameterDefault](p: A => Boolean): DResultG[D, e, A] = 
+            flatMap(a => if (p(a)) (d => Okay(a)) else (d => FailedG("filter failure", implicitly[FailedParameterDefault[e]].default)))
+
+        def flatten[DD <: D, e >: E, B](implicit ev: A => DResultG[DD, e, B]): DResultG[DD, e, B] = 
+            flatMap(ev)
+
+        def |[F, a >: A](f: FailedG[E] => ResultG[F, a]): DResultG[D, F, a] = 
+            d => fa(d) | f
+
+        def run[DD <: D](dd: DD): ResultG[E, A] = fa(dd)
+
+        def toOption[DD <: D, a >: A]: DD => Option[a] = 
+            dd => fa(dd).toOption
+
+        def toList[DD <: D, a >: A]: DD => List[a] = 
+            dd => fa(dd).toList
+    }
+
+    object instances {
+    	
+    }
+}
